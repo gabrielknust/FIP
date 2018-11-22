349d47df02100619071ad81cf1846086d2a8347f
JuanvictorO <juanvictoroliveira2009@gmail.com>
2018-11-22 17:16:35 -0200

    Update Poste.php

--- model/Poste.php
+++ model/Poste.php
@@ -1,28 +1,25 @@
 <?php
 
-class Poste
-{
-    private $foto;
-    private $numeracao;
-    
-    public function getFoto()
-    {
-        return $this->foto;
-    }
 
-    public function getNumeracao()
-    {
-        return $this->numeracao;
-    }
+class Poste{
+	private $foto;
+	private $numeracao;
 
-    public function setFoto($foto)
+	public function getFoto()
     {
-        $this->foto = $foto;
-    }
+		return $this->foto;
+	}
+	public function setFoto($foto)
+    {
+		$this->foto = $foto;
+	}
 
-    public function setNumeracao($numeracao)
+	public function getNumeracao(){
+        
+		return $this->numeracao;
+	}
+	public function setNumeracao($numeracao)
     {
-        $this->numeracao = $numeracao;
-    }
-  
+		$this->numeracao = $numeracao;
+	}
 }
